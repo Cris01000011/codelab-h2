7bc4a2424e28238d9b33de79df8f8d0a0fbf57f7
Cris01000011 <168916184+Cris01000011@users.noreply.github.com>
2024-06-28 13:33:36 -0600

    test bd with h2

---
+++ src/main/java/com/example/codelab_h2/CodelabH2Application.java
@@ -0,0 +1,13 @@
+package com.example.codelab_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelabH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelabH2Application.class, args);
+	}
+
+}
